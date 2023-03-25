4eb60e55e2664514bf9ba78f35a23d635b89b88b
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-03-25 19:32:41 +0100

    Update Premium.lua

--- katerhub/users/Premium.lua
+++ katerhub/users/Premium.lua
@@ -1,9 +1,9 @@
 katerhubprem = {
-		3932155354, -- Colin (adibaiwbod123)
-		620731643, -- 100NotFound (OG)
-		1500467286, -- Wojtek
-		2548112369, -- blendi
-		282180782, -- ceren (das ding ist sie würde jedem loch geben!)
+	3932155354, -- Colin (adibaiwbod123)
+	620731643, -- 100NotFound (OG)
+	1500467286, -- Wojtek
+	2548112369, -- blendi
+	282180782, -- ceren (das ding ist sie würde jedem loch geben!)
 }
 
 return katerhubprem
