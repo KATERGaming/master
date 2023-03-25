03800370c2d4a0c6714b17c5775db0734df4c751
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-03-25 19:19:54 +0100

    Update Admins.lua

--- katerhub/users/Admins.lua
+++ katerhub/users/Admins.lua
@@ -1,12 +1,12 @@
 katerhubadmins = {
-		2392979691, -- Colin
-		2221259006, -- Mert
-		2752924063, -- Rohan
-		151922487, -- Nikita
-		2207719666, -- Sefi
-		277059340, -- berat
-		935862376, -- Arthur Esau
-		659424770, -- Colin (greygerskill12)
+	2392979691, -- Colin
+	2221259006, -- Mert
+	2752924063, -- Rohan
+	151922487, -- Nikita
+	2207719666, -- Sefi
+	277059340, -- berat
+	935862376, -- Arthur Esau
+	659424770, -- Colin (greygerskill12)
 }
 
 return katerhubadmins
