d5aa0f81fc34750fce32ab8dc9decd166ace6da2
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-03-28 13:36:42 +0200

    Update blacklist.lua

--- katerhub/users/blacklist.lua
+++ katerhub/users/blacklist.lua
@@ -6,7 +6,7 @@ BlacklistedUsers = {
  1165714637, -- Abdelhhhh700
  4087001921, -- AlexLoLUw_xdl
  4416938063, -- ROBLOX9876523836268
- 3026491929,
+ 3026491929, -- Ragdoll fling guy
  115836014, -- Team_c00lkidds
  1164631419, -- spooky
  1922990593, -- goofy ahh radio guy
