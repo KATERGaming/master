3f70b4092c1db1d00d00a3f63c9ecb1d3e323333
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-03-28 13:35:34 +0200

    Update blacklist.lua

--- katerhub/users/blacklist.lua
+++ katerhub/users/blacklist.lua
@@ -1,15 +1,15 @@
 BlacklistedUsers = {
-	1910797347, -- Unlisted
-	1056305807, -- Unlisted
-	4061794726, -- Unlisted
-	1971882395, -- Unlisted
-	1165714637, -- Abdelhhhh700
-	4087001921, -- AlexLoLUw_xdl
-	4416938063, -- ROBLOX9876523836268
-	3026491929,
-	115836014, -- Team_c00lkidds
-	1164631419, -- spooky
-	1922990593, -- goofy ahh radio guy
+ 1910797347, -- Unlisted
+ 1056305807, -- Unlisted
+ 4061794726, -- Unlisted
+ 1971882395, -- Unlisted
+ 1165714637, -- Abdelhhhh700
+ 4087001921, -- AlexLoLUw_xdl
+ 4416938063, -- ROBLOX9876523836268
+ 3026491929,
+ 115836014, -- Team_c00lkidds
+ 1164631419, -- spooky
+ 1922990593, -- goofy ahh radio guy
 }
 
-return BlacklistedUsers
+return BlacklistedUsers;
