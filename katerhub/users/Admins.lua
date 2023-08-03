a284103dc169da1c9cd9a2b9d47b4e79a62ea2b7
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-08-04 01:45:08 +0200

    Create Admins.lua

---
+++ katerhub/users/Admins.lua
@@ -0,0 +1,14 @@
+katerhubadmins = {
+	2392979691, -- Kater 1
+	3932155354, -- Kater 3
+	2221259006, -- Loveri
+	2752924063, -- R0hambo
+	151922487, -- TylerJ
+	2207719666, -- Sefi
+	277059340, -- berat
+  935862376, -- 24KDrips
+  659424770, -- Colin (greygerskill12)
+  1448750441, -- Ben gamingde
+  717521694, -- Kater 2
+  1103530540 -- 24KDrips 2 acc
+}
