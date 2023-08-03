571bb94879406d1a4871303ba600455f021ad904
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-08-04 01:45:35 +0200

    Update Admins.lua

--- katerhub/users/Admins.lua
+++ katerhub/users/Admins.lua
@@ -6,9 +6,11 @@ katerhubadmins = {
 	151922487, -- TylerJ
 	2207719666, -- Sefi
 	277059340, -- berat
-  935862376, -- 24KDrips
-  659424770, -- Colin (greygerskill12)
-  1448750441, -- Ben gamingde
-  717521694, -- Kater 2
-  1103530540 -- 24KDrips 2 acc
+  	935862376, -- 24KDrips
+  	659424770, -- Colin (greygerskill12)
+  	1448750441, -- Ben gamingde
+  	717521694, -- Kater 2
+  	1103530540 -- 24KDrips 2 acc
 }
+
+return katerhubadmins;
