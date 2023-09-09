a590e91ff66a600c176b977f3c090e6927c886f1
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-09-10 00:13:09 +0200

    Connected every account

--- katerhub/users/Kater.lua
+++ katerhub/users/Kater.lua
@@ -1,5 +1,9 @@
 katerhimselfe = {
-	111,
+	2392979691, -- xxMr_Unknownxxxx
+	3932155354, -- adibaiwbod123
+  	659424770, -- greygerskill12
+  	717521694, -- Greygerskill1234
+	479703471, -- Greygerskill
 }
 
 return katerhimselfe;
