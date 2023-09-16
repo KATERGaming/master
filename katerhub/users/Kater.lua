67193db4558132ac35529a2425640b2763eb5131
Kater <91205706+KaterHub@users.noreply.github.com>
2023-09-16 21:56:02 +0200

    Removed katers list

--- katerhub/users/Kater.lua
+++
@@ -1,9 +0,0 @@
-katerhimselfe = {
-	2392979691, -- xxMr_Unknownxxxx
-	3932155354, -- adibaiwbod123
-  	659424770, -- greygerskill12
-  	717521694, -- Greygerskill1234
-	479703471, -- Greygerskill
-}
-
-return katerhimselfe;
