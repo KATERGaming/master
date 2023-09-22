e886d5fa3f8b2da23df2577f95584ad78e771acd
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-09-22 21:38:39 +0200

    Added Blacklisted games
    very bad games 😡

--- katerhub/misc/blacklistedgames.lua
+++ katerhub/misc/blacklistedgames.lua
@@ -1,5 +1,10 @@
 blacklistedgames = {
-  000,
+  3527629287, -- Big paintball
+  286090429, -- Arsenal
+  6678877691, -- Gay katana game
+  5938036553, -- Frontlines
+  4991214437, -- town
+  6243699076, -- the mimic
 }
 
-return
+return blacklistedgames;
