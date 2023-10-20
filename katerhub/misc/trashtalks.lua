f5f3b519dafbab5711daeaf87c2a75aa73a1d67a
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-20 21:52:53 +0200

    Added custom Trashtalking

---
+++ katerhub/misc/trashtalks.lua
@@ -0,0 +1,25 @@
+trashtalk = {
+  "fear me, not god",
+  "trash",
+  "ez",
+  "embarrassing",
+  "get better kid",
+  "were yo dad at?",
+  "suck",
+  "Noah's ark couldn't carry you",
+  "just stop playing",
+  "just leave lol",
+  "get better kid",
+  "the value of you is -inf",
+  "keep crying",
+  "ez kid",
+  "Worthless child",
+  "L",
+  "you listen to drake",
+  "Did you fall down the stairs?",
+  "If there would be a K\D in this game i wonder what would be yours.",
+  "are you still in kindergarden?",
+  "watch yo steps or imma wipe you lil bro 🔥🙏"
+}
+
+return trashtalk;
