417dae1cff7400023525282de1967a385289de17
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-21 23:48:55 +0200

    Update trashtalks.lua

--- katerhub/misc/trashtalks.lua
+++ katerhub/misc/trashtalks.lua
@@ -1,24 +1,8 @@
 trashtalk = {
-  "fear me, not god",
-  "trash",
-  "ez",
-  "embarrassing",
-  "get better kid",
-  "were yo dad at?",
-  "suck",
-  "Noah's ark couldn't carry you",
-  "just stop playing",
-  "just leave lol",
-  "get better kid",
-  "the value of you is -inf",
-  "keep crying",
-  "ez kid",
-  "Worthless child",
-  "L",
-  "you listen to drake",
-  "Did you fall down the stairs?",
-  "If there would be a K\D in this game i wonder what would be yours.",
-  "are you still in kindergarden?",
+  "you havnt washed yo boothy 😡✌!"
+  "ez kid 😎😎👍"
+  "yo mum fell down the stairs 🤩👌"
+  "You eat horse meat 🙏😢"
   "watch yo steps or imma wipe you lil bro 🔥🙏"
 }
 
