fd8facb546a98fbdce500bac653584d34b923d2e
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-22 00:06:44 +0200

    Update blacklist.lua

--- katerhub/users/blacklist.lua
+++ katerhub/users/blacklist.lua
@@ -4,6 +4,7 @@ BlacklistedUsers = {
   3380269507,
   4682027051,
   3091023538, -- random ass dahood furry lookin ass kys fucking has no parents hang yourselfe i hope your parents dont love you and you die because of depresion.
+  717521694, -- test
 }
 
 return BlacklistedUsers;
