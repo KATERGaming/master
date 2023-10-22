939a778a8c10b22157b595c4373f80a4de815705
Kater <91840948+Colin4President@users.noreply.github.com>
2023-10-22 12:50:46 +0200

    Update trashtalks.lua

--- katerhub/misc/trashtalks.lua
+++ katerhub/misc/trashtalks.lua
@@ -1,9 +1,15 @@
 trashtalk = {
-  "you havnt washed yo boothy 😡✌!"
-  "ez kid 😎😎👍"
-  "yo mum fell down the stairs 🤩👌"
-  "You eat horse meat 🙏😢"
-  "watch yo steps or imma wipe you lil bro 🔥🙏"
+  "you havnt washed yo boothy 😡✌!",
+  "ez kid 😎😎👍",
+  "yo mum fell down the stairs 🤩👌",
+  "You eat horse meat 🙏😢",
+  "watch yo steps or imma wipe you lil bro 🔥🙏",
+  "⭐ gorillas play with ma ballazz ⭐",
+  "Keep crying about you banning me 😢😢",
+  "You mad 😘",
+  "get better kid",
+  "Trash",
+  "i hate 🐱‍👤s!"
 }
 
 return trashtalk;
