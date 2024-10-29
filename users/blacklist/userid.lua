e57edcde1b71670e0fb574af1902bbcf8a3a1dcf
Kater <91840948+Colin4President@users.noreply.github.com>
2024-10-29 11:46:44 -0700

    Update userid.lua

--- users/blacklist/userid.lua
+++ users/blacklist/userid.lua
@@ -1,13 +1,13 @@
 users = {
   1344734565,
-  3091023538,
-  4682027051,
+  3091023538, -- Roblox Banned
+  4682027051, -- Roblox Banned
   4824922591,
-  3380269507,
-  4682027051,
-  3109367271,
-  3188110682,
-  2295939288,
+  3380269507, -- Stupid Ragdoll engine wannabe mod (GAY NIGGA)
+  4682027051, -- Roblox Banned
+  3109367271, -- Roblox Banned
+  3188110682, -- flame der nega
+  2295939288, -- dumb bitch
 }
 
 return users;
