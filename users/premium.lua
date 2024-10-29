cc3b9d22545803d8610f64967b6f5070372efd64
Farells <91205706+KATERGaming@users.noreply.github.com>
2024-10-29 10:57:18 -0700

    Update premium.lua

--- users/premium.lua
+++ users/premium.lua
@@ -1,1 +1,38 @@
-
+return {
+	3932155354, -- Colin (adibaiwbod123)
+	620731643, -- 100NotFound (OG)
+	1500467286, -- Wojtek (OG)
+	2548112369, -- Farted kid
+	--282180782, -- ceren (GAY)
+	433526031, -- 1Fo (Fortnite Streamer)
+	3741914212, -- Zurly808
+	1527694478, -- Ninja
+	2405228342, -- TheEmanurlERMBOY
+	2033262439, -- 42Q_user
+	4706077003, -- byfronWho_L
+	4710783998, --byfronWho_LL
+	1464911478, -- Drillz gf (Uwu love)
+	618940391, -- dollmannis
+	4642947692, -- (dahood gamer girl uwu)
+	2497941915, -- Jimmy
+	3956745069, -- jimmy 2
+	--3088440704, -- asiantbh
+	4476143468, -- byklexi
+	3409605853, -- 23_lolan (763467983493660683)
+	5113868874, -- vitor playstation
+	4045519446, -- vitor pc
+	813990562, -- taco
+	2566895126, -- Ragdoll guy
+	1894037343, -- miami
+	1163489700, -- drillz sister
+	--3109345494, -- Luv
+	2653637152, -- KenCarson (Dahood Guy)
+	3214835882, -- grave
+	155496675, -- mangooo
+	4291323787,
+	319475128,
+	4024696510,
+	7041580464, -- htcvicepro
+	1315079405, -- margo
+	7344674424, -- nigga acc
+};
