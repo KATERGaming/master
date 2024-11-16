85ccee99fc47dfc9f345232bacbcaec0723c967a
Kater <91840948+Colin4President@users.noreply.github.com>
2024-11-16 14:50:09 +0100

    Update userid.lua

--- users/blacklist/userid.lua
+++ users/blacklist/userid.lua
@@ -8,6 +8,9 @@ users = {
   3109367271, -- Roblox Banned
   3188110682, -- flame der nega
   2295939288, -- dumb bitch
+  3049958953, -- lasbian hoe (13)
+  2910181102, -- lasbian hoe (13)
+  1544263490, -- lasbian hoe (13)
 }
 
 return users;
