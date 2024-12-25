21a2ce5c66c4fc4c9b6a6f2dc0da3e518069d5e1
Kater <91840948+Colin4President@users.noreply.github.com>
2024-12-25 03:20:01 +0100

    Update premium.lua

--- users/premium.lua
+++ users/premium.lua
@@ -37,4 +37,5 @@ return {
 	378420866,
 	2910181102, -- lesbian alex donator  [[907344673616584744]]
 	437757950, -- purgedriot (gave it to him against his own will) [[511514390491693056]]
+	7344674424, -- Alt acc
 };
