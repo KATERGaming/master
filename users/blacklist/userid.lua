8d22de233308a7ebf8df6ff5f1f3bc27d1047787
Kater <91840948+Colin4President@users.noreply.github.com>
2025-01-27 22:24:34 +0100

    Update userid.lua

--- users/blacklist/userid.lua
+++ users/blacklist/userid.lua
@@ -13,6 +13,8 @@ users = {
   1464911478, -- Drillz gf (Uwu love)
   2792113770, -- brin
   411731161, -- Stupid ass Hood Modded nigga with no parents (is a fucking slut that keeps stomping people and saying the are usin lock, please rape her like her father did)
+  528474124, -- Dahood // "Dont leave" (Gets fucking killed over 15 times)
+  529113116, -- Dahood // "Go back to work nga" (dumb fucking hoe gets beaten by her boyfriend)
 }
 
 return users;
