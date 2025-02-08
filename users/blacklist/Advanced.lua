44b5800e84b311fb5a80144575868b9d67d45078
Kater <91840948+Colin4President@users.noreply.github.com>
2025-02-08 12:14:37 +0100

    Update Advanced.lua

--- users/blacklist/Advanced.lua
+++ users/blacklist/Advanced.lua
@@ -1,7 +1,19 @@
 return {
-  [1147757113] = "nigga";
-  [9] = "hi";
+  [1147757113] = "your just a stupid ass kid";
   [1344734565] = "bitchass nigga scaming little kids in pls donate + is a woman (kys please)";
   [3091023538] = "stupid ass furry killyourselfe please dumbnigga your parents hate you because you are gay you dumb fucking bitch (just please killyourselfe).";
-  [411731161] = "blacklisted for beeing the fucking slut she fucking is. please get fucking rapped nigga";
+  
+  [4682027051] = "."; -- Roblox Banned
+  [4824922591] = ".";
+  [3380269507] = "."; -- Stupid Ragdoll engine wannabe mod (GAY NIGGA)
+  [3109367271] = "."; -- Roblox Banned
+  [3188110682] = ".";
+  [2295939288] = "tiebonesteak my balls 83.85.57.222";
+  [3049958953] = "."; -- lasbian hoe (13)
+  [1544263490] = "."; -- lasbian hoe (13)
+  [1464911478] = ".";
+  [2792113770] = ".";
+  [411731161] = "Stupid ass Hood Modded nigga with no parents (is a fucking slut that keeps stomping people and saying the are usin lock, please rape her like her father did)";
+  [528474124] = '"Dont leave" (Gets fucking killed over 15 times)'; -- Dahood
+  [529113116] = '"Go back to work nga" (dumb fucking hoe gets beaten by her boyfriend)'; -- Dahood
 }
