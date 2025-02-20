1a10d1d0d7c7c1b2b655cb58d7c5bec35ec27e43
Colin <91840948+Colin4President@users.noreply.github.com>
2025-02-20 17:58:21 +0100

    Update AdvancedList.lua

--- users/blacklist/AdvancedList.lua
+++ users/blacklist/AdvancedList.lua
@@ -1,23 +1,27 @@
 return {
-  [1147757113] = "your just a stupid ass kid";
-  [1344734565] = "bitchass nigga scaming little kids in pls donate + is a woman (kys please)";
+  [1147757113] = "just a stupid ass kid";
+  [1344734565] = ".";
   [3091023538] = "stupid ass furry killyourselfe please dumbnigga your parents hate you because you are gay you dumb fucking bitch (just please killyourselfe).";
-  [4682027051] = "."; -- Roblox Banned
+  [4682027051] = ".";
   [4824922591] = ".";
-  [3380269507] = "."; -- Stupid Ragdoll engine wannabe mod (GAY NIGGA)
-  [3109367271] = "."; -- Roblox Banned
+  [3380269507] = ".";
+  [3109367271] = ".";
   [3188110682] = ".";
   [2295939288] = "tiebonesteak my balls 83.85.57.222";
-  [3049958953] = "."; -- lasbian hoe (13)
-  [1544263490] = "."; -- lasbian hoe (13)
+  [3049958953] = ".";
+  [1544263490] = ".";
   [1464911478] = ".";
   [2792113770] = ".";
   [411731161] = "Stupid ass Hood Modded nigga with no parents (is a fucking slut that keeps stomping people and saying the are usin lock, please rape her like her father did)";
-  [528474124] = '"Dont leave" (Gets fucking killed over 15 times)'; -- Dahood
-  [529113116] = '"Go back to work nga" (dumb fucking hoe gets beaten by her boyfriend)'; -- Dahood
+  [528474124] = '"Dont leave" (Gets fucking killed over 15 times)';
+  [529113116] = '"Go back to work nga" (dumb fucking hoe gets beaten by her boyfriend)';
   [2399670479] = "biggest fucking skid (Owner of Spanish hub aka Gay Hub)";
   [514801018] = "nigga has a gay simp friend (nga thinks shes god with him)\n\n edit: that a twink";
-  [1359639821] = '"whats a father whats that" ahh stupid ass nigga.'
+  [1359639821] = '"whats a father whats that" ahh stupid ass nigga.';
+  [1082663822] = ".";
+  [2219179758] = 'said "ohh alr watch" in an attempt to kill me (he got fucking executed in shame).';
+  [7111160570] = 'said "ohh alr watch" in an attempt to kill me (he got fucking executed in shame).';
+  [7070266650] = '.';
   
   ["00000-00000-000"] = ".";
   ["83.85.57.222"] = ".";
