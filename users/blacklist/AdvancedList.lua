c10585fbb74adea80b3b6430302bb82e9c9dd481
Colin <91840948+Colin4President@users.noreply.github.com>
2025-06-10 14:17:06 +0200

    Update AdvancedList.lua

--- users/blacklist/AdvancedList.lua
+++ users/blacklist/AdvancedList.lua
@@ -41,6 +41,7 @@ return {
   [3145512471] = "make my script free";
   [7544916799] = "sorry we dont accept gay users in our community.";
   [1233515660] = nil;
+  [1596990522] = nil;
   
   ["00000-00000-000"] = ".";
 }
